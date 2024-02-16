3aa0ca9301f6f1b85bec10c00756c90f01745f13
Adam McNeilly <amcneilly331@gmail.com>
2024-02-15 19:51:37 -0500

    Adding assets from Bloom.

--- app/src/main/java/com/adammcneilly/bloom/theme/Color.kt
+++ app/src/main/java/com/adammcneilly/bloom/theme/Color.kt
@@ -4,6 +4,11 @@ package com.adammcneilly.bloom.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Teal200 = Color(0xFF03DAC5)
+val White = Color(0xFFFFFFFF)
+val White150 = White.copy(alpha = 0.15F)
+val White850 = White.copy(alpha = 0.85F)
+val Pink100 = Color(0xFFFFF1F1)
+val Pink900 = Color(0xFF3F2C2C)
+val Gray = Color(0xFF232323)
+val Green300 = Color(0xFFB8C9B8)
+val Green900 = Color(0xFF2D3B2D)
